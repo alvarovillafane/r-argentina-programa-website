068af6cdd49743cd118ca339145a94c400012f14
Alvaro <alvaro.villafane-molina@collabforge.com>
2019-10-30 16:59:02 +1100

    Refactor: estado innecesario en clase removido

--- src/components/Clase.js
+++ src/components/Clase.js
@@ -3,36 +3,16 @@ import Typography from '@material-ui/core/Typography';
 import data from '../data/clases.json';
 
 class Clase extends React.Component {
-  state = {
-    clase: null,
-    error: null,
-    isLoading: true
-  };
-  
-    
-  componentDidMount() {
-    try {
-      let clase = data.clases[this.props.match.params.clase - 1];
-      if(!clase) { throw new Error(); } 
-      
-      this.setState({clase: clase, isLoading: false});
-    } catch (error) {
-      this.setState({ error, isLoading: false});
-    } 
-  }
-  
+
   render() {
-    const {error, clase, isLoading} = this.state;
-    if (error) { return <Typography variant="h6">Clase no encontrada</Typography> }
+    const id = this.props.match.params.clase - 1;
+    let clase = data.clases[id];
 
-    return !isLoading && <ClaseUI clase={clase}/>;
+    if(!clase) { return <Typography variant="h6">Clase no encontrada</Typography>; } 
+
+    return (
+      <Typography variant="h2" component="h1">{clase.titulo}</Typography>
+    )
   }
 }
-
-function ClaseUI({clase}){
-  return (
-    <Typography variant="h2" component="h1">{clase.titulo}</Typography>
-  )
-}
-
 export default Clase;
