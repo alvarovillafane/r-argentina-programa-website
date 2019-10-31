bca65eb8b7c25b9fb2979a4e0f1a79888788d58e
Alvaro <alvaro.villafane-molina@collabforge.com>
2019-11-01 10:59:54 +1100

    Agregado youtube id y componente a las clases

--- src/components/Clase.js
+++ src/components/Clase.js
@@ -1,18 +1,45 @@
 import React from 'react';
 import Typography from '@material-ui/core/Typography';
+import YouTube from 'react-youtube';
 import data from '../data/clases.json';
+import { withStyles } from '@material-ui/core/styles';
+import Card from '@material-ui/core/Card';
+import CardContent from '@material-ui/core/CardContent';
+
+const styles = theme => ({
+  youtube: {
+    padding: theme.spacing(3),
+    textAlign: 'center'
+  },
+});
 
 class Clase extends React.Component {
 
   render() {
-    const id = this.props.match.params.clase - 1;
+    const {match, classes} = this.props;
+    const id = match.params.clase - 1;
     let clase = data.clases[id];
+    const opts = {
+      height: '390',
+      width: '640',
+    };
 
     if(!clase) { return <Typography variant="h6">Clase no encontrada</Typography>; } 
 
     return (
-      <Typography variant="h2" component="h1">{clase.titulo}</Typography>
+      <React.Fragment>
+        <Typography variant="h2" component="h1">{clase.titulo}</Typography>
+        <Card className={classes.youtube}>
+          <CardContent>
+            <YouTube
+              videoId={clase.youtube_id}
+              opts={opts}
+            />
+          </CardContent>
+        </Card>
+      </React.Fragment>
     )
   }
 }
-export default Clase;
+
+export default withStyles(styles)(Clase);
