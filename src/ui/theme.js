335ac53e12050222db6aa5edc135024cf887b0a4
Alvaro <alvaro.villafane-molina@collabforge.com>
2019-11-01 15:22:13 +1100

    Actualizado css defaults

--- src/ui/theme.js
+++ src/ui/theme.js
@@ -1,10 +1,25 @@
 import blue from '@material-ui/core/colors/blue';
+import green from '@material-ui/core/colors/green';
+import blueGrey from '@material-ui/core/colors/blueGrey';
 import { createMuiTheme } from '@material-ui/core/styles';
+import { fontSize } from '@material-ui/system';
 
 export default createMuiTheme({
   palette: {
     primary: {
       main: blue[600]
+    },
+    secondary: {
+      main: green[400]
+    },
+    text: {
+      primary: blueGrey[900],
+      fontSize: '1rem'
+    },
+    typography: {
+      body1: {
+        fontSize: 16,
+      },
     },
   },
 });
