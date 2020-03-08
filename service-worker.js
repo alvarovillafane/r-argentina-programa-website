bc85b049f7f9d32d8ee56c99581ac383d93ff811
vmalvaro <alvarovm@gmail.com>
2020-03-08 21:54:44 +0000

    Deploying to gh-pages from  - 72b80490b3c426afa6c5a9c6f72503cdc2ff4bfa 🚀

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.52ea54f2dc4ce3f8e2e791340cb67919.js"
+  "/precache-manifest.1161359379237899178d91fbf2c13559.js"
 );
 
 self.addEventListener('message', (event) => {
