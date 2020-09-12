18d99e198e3427ada4b2810838ae49f4a5a807eb
vmalvaro <alvarovm@gmail.com>
2020-09-12 06:36:19 +0000

    Deploying to gh-pages from  @ 89e87616fba470bb339c09ecf06d0692538966c4 🚀

---
+++ precache-manifest.1366ab9caf454b8282f70f21e977a110.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "532fb2e616e4c16dac1e8750921b2616",
+    "url": "/index.html"
+  },
+  {
+    "revision": "b0175db85dfd5be1703b",
+    "url": "/static/css/main.5223903d.chunk.css"
+  },
+  {
+    "revision": "6afd28053e73b248a0a4",
+    "url": "/static/js/2.8dbe7150.chunk.js"
+  },
+  {
+    "revision": "943aec86c35bb3f39d18124af76a452f",
+    "url": "/static/js/2.8dbe7150.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b0175db85dfd5be1703b",
+    "url": "/static/js/main.07e02af9.chunk.js"
+  },
+  {
+    "revision": "93c217ebf0c2db4f6a21",
+    "url": "/static/js/runtime-main.80faec55.js"
+  },
+  {
+    "revision": "b70109da8b4b7eae306690c78f1cb5c2",
+    "url": "/static/media/logo.b70109da.jpg"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.62d19245111e8e5215cb32d8689c5fa6.js"
+  "/precache-manifest.1366ab9caf454b8282f70f21e977a110.js"
 );
 
 self.addEventListener('message', (event) => {
